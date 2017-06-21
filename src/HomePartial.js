11fb6f1ac1df6983ba72e6bb04a38c4315e636fc
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-06-21 16:53:08 -0700

    chat chat screen image to current

--- src/HomePartial.js
+++ src/HomePartial.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import map from './map.png';
+import chat_screen from './chat_screen.png';
 import './App.css';
 
 
@@ -10,13 +11,13 @@ export default class HomePartial extends Component {
 
         <div className="feature">
           <h2>Find Nearby Otakus</h2>
-          <div className="featureHalf"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+          <div className="featureHalf"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
           <div className="featureHalf"><img alt="" src={map}/></div>
         </div>
 
         <div className="feature">
           <h2>Send a chat request</h2>
-          <div className="featureHalf"><img alt="" src="http://fdietz.github.io/images/chitchat_components_2.png"/></div>
+          <div className="featureHalf"><img alt="" src={chat_screen}/></div>
           <div className="featureHalf"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
         </div>
 
